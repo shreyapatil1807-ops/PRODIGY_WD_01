848108575df1a9d68a3a31ec17abfc5872a100fc
shreyapatil1807-ops <shreyapatil1807@gmail.com>
2025-08-20 21:27:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbar = document.getElementById('navbar');
+const navLinks = document.getElementById('nav-links');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+function toggleMenu() {
+  navLinks.classList.toggle('show');
+}
